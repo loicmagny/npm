25052aaaf434f1b6297e855669cf5c34b250b659
elderopedia <loicmagny60@gmail.com>
2022-01-26 12:33:30 +0100

    ajout comm ur handler

--- handler.php
+++ handler.php
@@ -1,5 +1,5 @@
 <?php
-
+// Ce fichier gère les appels Ajax
 require_once 'models/database.php';
 require_once 'models/athletes_model.php';
 require_once 'models/categories_model.php';
@@ -11,9 +11,9 @@ require_once 'models/athFouls_model.php';
 require_once "assets/libs/simplexlsx-master/src/SimpleXLSX.php";
 require_once 'controllers/parser.php';
 require_once 'controllers/athletes_controller.php';
-
+// Quand l'appel est effectué
 if (isset($_POST['autoCall'])) {
-
+// En fonction du second paramètre de Ajax Call on sort le controller nécéssaire
     if (isset($_POST['file'])) {
         if ($_POST['file'] == 0) {
         }
@@ -29,9 +29,14 @@ if (isset($_POST['autoCall'])) {
             require 'controllers/fouls_controller.php';
         }
     }
+    // Si il y a un 3ème paramètre dans la fonction AjaxCall
     if (isset($_POST['arg'])) {
+        // On concatène le premier paramètre qui correspond au nom de la fonction dans le controller qu'on appelle avec le troisième, 
+        // qui est un tableau des paramètres de la fonction php dans le controller qu'on appelle
         echo json_encode($_POST['function']($_POST['arg']));
+        // Sinon
     } else {
+        // On concatène juste le premier paramètre AjaxCall, soit le nom de la fonction du controller appelé
         echo json_encode($_POST['function']());
     }
     unset($_POST);
